d699eecd64677684b70bfbaecd8c7539ba2971aa
sungdongyoon <ehdtjd0906@naver.com>
2023-10-24 13:54:29 +0900

    프로젝트 설명 수정

--- portfolio/src/pages/ProjectDetail.js
+++ portfolio/src/pages/ProjectDetail.js
@@ -49,7 +49,7 @@ const ProjectTitle = styled.div`
     margin-bottom: 15px;
   }
   span {
-    font-size: 1.2rem;
+    font-size: 1.5rem;
     color: var(--gray);
   }
 `;
@@ -78,7 +78,6 @@ const ProjectInfo = styled.div`
 const InfoBox = styled.div`
   background-color: var(--white);
   width: 100%;
-  height: 50vh;
   padding: 20px;
   border-radius: 10px;
 `;
@@ -137,173 +136,6 @@ const Detail = styled.div`
   }
 `;
 
-// const ProjectInfo = styled.div`
-//   width: 70%;
-//   display: flex;
-//   flex-direction: column;
-//   justify-content: center;
-//   align-items: center;
-//   .project_wrap {
-//     width: 100%;
-//     display: flex;
-//     flex-direction: column;
-//     gap: 50px;
-//     .project_content {
-//       width: 100%;
-//       display: flex;
-//       flex-direction: column;
-//       flex: 6;
-//       gap: 50px;
-//       margin-bottom: 50px;
-//       .project_title {
-//         display: flex;
-//         align-items: center;
-//         font-size: 3.5rem;
-//         font-weight: 700;
-//         padding-bottom: 10px;
-//         .project_team {
-//           background-color: var(--blue);
-//           border-radius: 50px;
-//           color: var(--white);
-//           font-size: 1rem;
-//           padding: 0px 10px;
-//           margin-left: 20px;
-//         }
-//       }
-//       span {
-//         font-size: 1.5rem;
-//         line-height: 20px;
-//       }
-//       .project_info {
-//         width: 100%;
-//       }
-//     }
-//     .project_links {
-//       width: 100%;
-//       display: flex;
-//       align-items: center;
-//       gap: 20px;
-//       flex: 1;
-//       span {
-//         background-color: var(--black);
-//         color: var(--white);
-//         font-size: 1.3rem;
-//         padding: 10px 20px;
-//         cursor: pointer;
-//       }
-//     }
-//     .project_tags {
-//       display: flex;
-//       align-items: center;
-//       flex-wrap: wrap;
-//       gap: 10px;
-//       flex: 1;
-//       span {
-//         background-color: var(--sky-blue);
-//         color: var(--black);
-//         font-size: 1.3rem;
-//         margin-right: 20px;
-//         padding: 10px 20px;
-//       }
-//     }
-//   }
-//   @media screen and ${responsive.laptop} {
-//     .project_wrap {
-//       .project_content {
-//         .project_title {
-//           font-size: 3rem;
-//         }
-//         span {
-//           font-size: 1.4rem;
-//         }
-//       }
-//       .project_links {
-//         span {
-//           font-size: 1.2rem;
-//         }
-//       }
-//       .project_tags {
-//         gap: 0px;
-//         span {
-//           font-size: 1.2rem;
-//         }
-//       }
-//     }
-//   }
-//   @media screen and ${responsive.tablet} {
-//     .project_wrap {
-//       .project_content {
-//         .project_title {
-//           font-size: 2.5rem;
-//           .project_team {
-//             font-size: 1.2rem;
-//           }
-//         }
-//       }
-//     } 
-//   }
-//   @media screen and ${responsive.mobile} {
-//     .project_wrap {
-//       gap: 30px;
-//       .project_links {
-//         span {
-//           font-size: 1rem;
-//           padding: 7px 15px;
-//         }
-//       }
-//       .project_tags {
-//         span {
-//           font-size: 1rem;
-//           padding: 7px 15px;
-//         }
-//       }
-//     }
-//   }
-//   @media screen and ${responsive.iphone12Pro} {
-//     .project_wrap {
-//       .project_content {
-//         span {
-//           font-size: 1.2rem;
-//         }
-//       }
-//       .project_tags {
-//         span {
-//           font-size: 1rem;
-//           padding: 7px 15px;
-//           &:last-child {
-//             margin-top: 10px;
-//           }
-//         }
-//       }
-//     }
-//   }
-// `;
-
-// const ProjectGif = styled.div`
-//   width: 70%;
-//   display: flex;
-//   flex-direction: column;
-//   align-items: center;
-//   gap: 50px;
-//   border-top: 3px solid var(--light-gray);
-//   padding-top: 50px;
-//   margin-bottom: 50px;
-//   span {
-//     font-size: 1.4rem;
-//   }
-//   img {
-//     width: 100%;
-//     border: 2px solid var(--light-gray);
-//   }
-//   @media screen and ${responsive.tablet} {
-//     height: 50%;
-//   }
-//   @media screen and ${responsive.iphone12Pro} {
-//     span {
-//       font-size: 1.2rem;
-//     }
-//   }
-// `;
 
 const ProjectDetail = () => {
   const { id } = useParams();
@@ -327,7 +159,6 @@ const ProjectDetail = () => {
     navigate('/work');
   }
 
-  const arr = [0, 1, 2];
   return (
     <Container>
       <FontAwesomeIcon onClick={goWork} className='backBtn' icon={faChevronLeft}/>
@@ -387,45 +218,3 @@ const ProjectDetail = () => {
 }
 
 export default ProjectDetail;
-
-
-
-
-
-
-
-
-
-
-          {/* <ProjectInfo>
-            <div className='project_wrap'>
-              <div className='project_content'>
-                <h1 className='project_title'>
-                  {it.name}
-                  {it.team && <span className='project_team'>team project</span>}
-                </h1>
-                <span className='project_info'>
-                  {it.description}
-                </span>
-              </div>
-              <div className='project_links'>
-                <Link target='_blank' to={it.link}>
-                  <span>VIEW WEBSITE</span>
-                </Link>
-                <Link target='_blank' to={it.github}>
-                  <span>GitHub</span>
-                </Link>
-              </div>
-              <div className='project_tags'>
-                {it.tag.map((tag) => (
-                  <span>{tag}</span>
-                ))}
-              </div>
-            </div>
-          </ProjectInfo>
-          {arr.map((el) => (
-            <ProjectGif>
-              <span>{it.subDesc[el]}</span>
-              <img src={getProjectDetailGif(it.gifNum[el])}/>
-            </ProjectGif>
-          ))} */}
